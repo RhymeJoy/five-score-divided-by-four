d9df8d36fc5881e8d4cd4890237fe272fe43441f
欣韻 <t40932203@gmail.com>
2025-01-16 00:24:39 +0800

    Update chapters-menu.js

--- The-Vapor-of-Water/JS/chapters-menu.js
+++ The-Vapor-of-Water/JS/chapters-menu.js
@@ -2,7 +2,7 @@ const chapters = [
     { title: "作者前言", href: "index.html" },
     { title: "1.第一章：南港小馬展",   href: "Chapter1.html" },
     { title: "2.第二章：身世謎題[上]", href: "Chapter2.html" },
-    // { title: "3.第三章：身世謎題[下]", href: "Chapter3.html" },
+    { title: "3.第三章：身世謎題[中]", href: "Chapter3.html" },
 ];
 
 const chapterList = document.getElementById('chapterList');
