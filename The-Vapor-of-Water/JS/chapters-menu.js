06fa3509694bb72cdd70d331c40d0dcdeb5424c5
欣韻 <t40932203@gmail.com>
2025-02-13 23:10:27 +0800

    Add files via upload

--- The-Vapor-of-Water/JS/chapters-menu.js
+++ The-Vapor-of-Water/JS/chapters-menu.js
@@ -1,41 +1,47 @@
 const chapters = [
-    { title: "作者前言", href: "index.html" },
-    { title: "1.第一章：南港小馬展",        href: "Chapter1.html" },
-    { title: "2.第二章：身世謎題[上]",      href: "Chapter2.html" },
-    { title: "3.第三章：身世謎題[中]",      href: "Chapter3.html" },
-    { title: "4.第四章：身世謎題[下]",      href: "Chapter4.html" },
-    { title: "5.第五章：異變日增[上]",      href: "Chapter5.html" },
-    { title: "6.第六章：異變日增[下]",      href: "Chapter6.html" },
-    { title: "7.第七章：空中之旅",          href: "Chapter7.html" },
-    { title: "8.第八章：無盡的噩夢",        href: "Chapter8.html" },
-    // { title: "9.第九章：溫哥華",         href: "Chapter9.html" },
-    // { title: "10.第十章：",     href: "Chapter10.html" },
-    // { title: "11.第十一章：",   href: "Chapter11.html" },
-    // { title: "12.第十二章：",   href: "Chapter12.html" },
-    // { title: "13.第十三章：",   href: "Chapter13.html" },
-    // { title: "14.第十四章：",   href: "Chapter14.html" },
-    // { title: "15.第十五章：",   href: "Chapter15.html" },
-    // { title: "16.第十六章：",   href: "Chapter16.html" },
-    // { title: "17.第十七章：",   href: "Chapter17.html" },
-    // { title: "18.第十八章：",   href: "Chapter18.html" },
-    // { title: "19.第十九章：",   href: "Chapter19.html" },
-    // { title: "20.第二十章：",   href: "Chapter20.html" },
-    // { title: "21.第二十一章：", href: "Chapter21.html" },
-    // { title: "22.第二十二章：", href: "Chapter22.html" },
-    // { title: "23.第二十三章：", href: "Chapter23.html" },
-    // { title: "24.第二十四章：", href: "Chapter24.html" },
-    // { title: "25.第二十五章：", href: "Chapter25.html" },
+    { title: "作者前言",                 href: "index.html" },
+    { title: "第一章：南港小馬展",        href: "Chapter1.html" },
+    { title: "第二章：身世謎題[上]",      href: "Chapter2.html" },
+    { title: "第三章：身世謎題[中]",      href: "Chapter3.html" },
+    { title: "第四章：身世謎題[下]",      href: "Chapter4.html" },
+    { title: "第五章：異變日增[上]",      href: "Chapter5.html" },
+    { title: "第六章：異變日增[下]",      href: "Chapter6.html" },
+    { title: "第七章：空中之旅",          href: "Chapter7.html" },
+    { title: "第八章：無盡的噩夢",        href: "Chapter8.html" },
+    { title: "第九章：溫哥華",            href: "Chapter9.html" },
+    // { title: "第十章：手稿的意義",     href: "Chapter10.html" },
+    // { title: "第十一章：",   href: "Chapter11.html" },
+    // { title: "第十二章：",   href: "Chapter12.html" },
+    // { title: "第十三章：",   href: "Chapter13.html" },
+    // { title: "第十四章：",   href: "Chapter14.html" },
+    // { title: "第十五章：",   href: "Chapter15.html" },
+    // { title: "第十六章：",   href: "Chapter16.html" },
+    // { title: "第十七章：",   href: "Chapter17.html" },
+    // { title: "第十八章：",   href: "Chapter18.html" },
+    // { title: "第十九章：",   href: "Chapter19.html" },
+    // { title: "第二十章：",   href: "Chapter20.html" },
+    // { title: "第二十一章：", href: "Chapter21.html" },
+    // { title: "第二十二章：", href: "Chapter22.html" },
+    // { title: "第二十三章：", href: "Chapter23.html" },
+    // { title: "第二十四章：", href: "Chapter24.html" },
+    // { title: "第二十五章：", href: "Chapter25.html" },
 ];
 
 const chapterList = document.getElementById('chapterList');
 const chapterLinks = []; // 用於存儲所有章節鏈接
 
 // 動態插入章節
-chapters.forEach(chapter => {
+chapters.forEach((chapter, index) => {
     const listItem = document.createElement('li');
     const link = document.createElement('a');
     link.href = chapter.href;
-    link.textContent = chapter.title;
+    
+    // 除「作者前言」外的章節加上編號
+    if (index > 0) {
+        link.textContent = `${index}.${chapter.title}`;
+    } else {
+        link.textContent = chapter.title;
+    }
 
     listItem.appendChild(link);
     chapterList.appendChild(listItem);
@@ -49,7 +55,6 @@ chapterLinks.forEach(link => {
         link.classList.add('selected'); // 為當前頁面對應的鏈接添加選中樣式
     }
 });
-
 
 // 選取上一章和下一章按鈕
 const prevButton = document.getElementById('prevChapter');
@@ -82,12 +87,10 @@ if (currentChapterIndex < chapters.length - 1) {
 }
 
 // footer
-
 const footer = document.createElement('footer');
 
 const paragraph = document.createElement('p');
 paragraph.innerHTML = '&copy; 2024-2025 欣韻 , All rights reserved.';
 
 footer.appendChild(paragraph);
-
 document.body.appendChild(footer);
